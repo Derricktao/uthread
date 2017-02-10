8d038b8f943d471e92af2238e634105e035b210e
Alvis Leung <alvleung@ucdavis.edu>
2017-02-10 01:53:06 -0800

    Queue test file, needs to be completed

---
+++ test-queue.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <queue.h>
+
+int main(int argc, char* argv[])
+{
+	queue_t queue = queue_create();
+
+	int a = 1;
+	int b = 2;
+	int c = 3;
+	int d = 4;
+	
+	int *e;
+	
+	printf("%o %o %o\n", &a, &b, &e);
+
+	queue_enqueue(queue,&a);
+	queue_enqueue(queue,&b);
+	//queue_enqueue(queue,&c);
+	//queue_enqueue(queue,&d);
+	
+	queue_dequeue(queue,(void**)&e);
+	printf("%o %o\n", e, &a);
+	
+	queue_dequeue(queue,(void**)&e);
+	printf("%o %o\n", e, &b);	
+	
+	//queue_delete(queue,&b);
+
+	return 0;
+}
